ebfcbe58eb2de61523a51dcf702add522bbe52b2
Jeffrey Brite <jeff@techcollective.com>
2023-06-08 12:33:15 -0500

    Created a tempate config file

---
+++ config_template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.9
+
+# Unifi
+UnifiUsername = ''
+UnifiPassword = ''
+UnifiHost = ''
+UnifiPort = ''
+
+# Autotask
+atUsername = ''
+atPassword = ''
+atAPIInterationcode = ''
+atHost = ''
+
+atCIType4network =   # This is my "Networking Devices" Type
+atCICategory =   # This is my "Unifi Controller Devices" catagory
+atProductID =   # Can get from the URL in Autotask
+
+unifi_ignore = ("")
